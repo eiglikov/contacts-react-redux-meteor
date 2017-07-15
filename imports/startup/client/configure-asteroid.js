d519f75c163938ecf77fb9ec754f8724b430c210
Erik Iglikov <sneanone@gmail.com>
2017-07-15 17:30:15 -0400

    update ws link

--- imports/startup/client/configure-asteroid.js
+++ imports/startup/client/configure-asteroid.js
@@ -3,7 +3,7 @@ import { createClass } from 'asteroid'
 const Asteroid = createClass()
 // Connect to a Meteor backend
 const asteroid = new Asteroid({
-  endpoint: 'ws://contacts-meteor.herokuapp.com',
+  endpoint: 'ws://contacts-meteor.herokuapp.com/websocket',
 })
 // endpoint: 'ws://contacts-meteor.herokuapp.com',
 // endpoint: 'ws://localhost:3000/websocket',
