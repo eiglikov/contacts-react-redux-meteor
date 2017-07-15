91fabc64aa110810a15f873766b7b9c92f9951d5
Erik Iglikov <sneanone@gmail.com>
2017-07-15 18:38:27 -0400

    fixed groupSelector

--- imports/startup/client/configure-asteroid.js
+++ imports/startup/client/configure-asteroid.js
@@ -3,8 +3,8 @@ import { createClass } from 'asteroid'
 const Asteroid = createClass()
 // Connect to a Meteor backend
 const asteroid = new Asteroid({
-  endpoint: 'ws://localhost:3000/websocket',
+  endpoint: 'wss://contacts-meteor.herokuapp.com/websocket',
 })
-// endpoint: 'wss://contacts-meteor.herokuapp.com',
+// endpoint: 'wss://contacts-meteor.herokuapp.com/websocket',
 // endpoint: 'ws://localhost:3000/websocket',
 export default asteroid
