a84998259ea82e075eac77fc2250c841e7c921ef
Erik Iglikov <sneanone@gmail.com>
2017-07-20 02:53:05 -0400

    fixed WS changing issue

--- imports/startup/client/configure-asteroid.js
+++ imports/startup/client/configure-asteroid.js
@@ -4,7 +4,6 @@ const Asteroid = createClass()
 // Connect to a Meteor backend
 const asteroid = new Asteroid({
   endpoint: 'wss://contacts-meteor.herokuapp.com/websocket',
+  // endpoint: 'ws://localhost:3000/websocket',
 })
-// endpoint: 'wss://contacts-meteor.herokuapp.com/websocket',
-// endpoint: 'ws://localhost:3000/websocket',
 export default asteroid
