1eb33b6790888b63c439a69bb1c6cd0e29f376af
Erik Iglikov <sneanone@gmail.com>
2017-07-20 03:04:59 -0400

    Revert "fixed WS changing issue"
    This reverts commit d51248812ef5095c97e902bd3bc826f97e8ba55b.

--- imports/startup/client/configure-asteroid.js
+++ imports/startup/client/configure-asteroid.js
@@ -1,17 +1,9 @@
 import { createClass } from 'asteroid'
-import ip from 'ip'
-
-console.log("LOCAL IP", ip.address());
-let address
-if (ip.address() == '127.0.0.1')
-  address = `ws://${ip.address()}:3000`
-else
-  address = `wss://${ip.address()}`
 
 const Asteroid = createClass()
 // Connect to a Meteor backend
 const asteroid = new Asteroid({
-  endpoint: `${address}/websocket`,
+  endpoint: 'ws://192.168.2.14:3000/websocket',
 })
 // endpoint: 'wss://contacts-meteor.herokuapp.com/websocket',
 // endpoint: 'ws://192.168.2.14:3000/websocket',
