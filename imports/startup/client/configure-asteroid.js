d51248812ef5095c97e902bd3bc826f97e8ba55b
Erik Iglikov <sneanone@gmail.com>
2017-07-20 02:53:05 -0400

    fixed WS changing issue

--- imports/startup/client/configure-asteroid.js
+++ imports/startup/client/configure-asteroid.js
@@ -1,9 +1,17 @@
 import { createClass } from 'asteroid'
+import ip from 'ip'
+
+console.log("LOCAL IP", ip.address());
+let address
+if (ip.address() == '127.0.0.1')
+  address = `ws://${ip.address()}:3000`
+else
+  address = `wss://${ip.address()}`
 
 const Asteroid = createClass()
 // Connect to a Meteor backend
 const asteroid = new Asteroid({
-  endpoint: 'ws://192.168.2.14:3000/websocket',
+  endpoint: `${address}/websocket`,
 })
 // endpoint: 'wss://contacts-meteor.herokuapp.com/websocket',
 // endpoint: 'ws://192.168.2.14:3000/websocket',
